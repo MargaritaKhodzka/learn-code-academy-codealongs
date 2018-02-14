c19899cb6af5ae5ae4d4511aac01eba5b693547f
MargaritaKhodzka <mkhodzka@gmail.com>
2018-02-13 21:05:44 -0500

    selectors

---
+++ selectors/scripts/main.js
@@ -0,0 +1,17 @@
+var pTags = document.getElementsByTagName('p')
+var firstPTag = pTags[0]
+
+document.getElementsByClassName('done')
+
+document.getElementById('checklist')
+
+document.querySelector('.done')
+
+document.querySelector('#checklist')
+
+document.querySelectorAll('p')
+
+firstPTag.innerHTML = "new paragraph <strong>content</strong>"
+
+var li  = document.querySelector('.done')
+li.className = ""
