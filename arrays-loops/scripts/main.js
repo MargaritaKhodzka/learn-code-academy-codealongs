7c3394b1239101361da45866e202ec56a49699f8
MargaritaKhodzka <mkhodzka@gmail.com>
2018-02-13 20:21:05 -0500

    arrays and loops

---
+++ arrays-loops/scripts/main.js
@@ -0,0 +1,24 @@
+var myList = ['apples', 'oranges', 'bananas']
+//
+// myList.forEach(function(value, index) {
+//   alert("I have " + value + " in my shopping bag")
+// })
+
+// var times = 0
+// while (times < 10) {
+//   console.log("tried it " + times + " times")
+//   times++
+// }
+
+// do {
+//   console.log("tried it " + times + " times")
+//   times++
+// } while (times < 10)
+
+// for (setup, comparison, change) {
+//
+// }
+
+for (var i = 0; i < myList.length; i++) {
+  console.log("I have " + myList[i] + " in my basket")
+}
