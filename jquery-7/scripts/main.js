439ecda5ee6f4b53e7c46e897d57daaaa1d81cb1
MargaritaKhodzka <mkhodzka@gmail.com>
2018-02-24 23:15:38 -0500

    ajax part 1

---
+++ jquery-7/scripts/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  var $orders = $('#orders')
+  $.ajax({
+    type: 'GET',
+    url: '/api/orders',
+    success: function(orders) {
+      $.each(orders, function(i, order) {
+        $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>')
+      })
+    }
+  })
+})
