cf3983327df35db1f5e5b8d52dc6f6d7a5a2be8d
MargaritaKhodzka <mkhodzka@gmail.com>
2018-02-25 18:27:48 -0500

    delete functionality

--- jquery-7-8-9-10/scripts/main.js
+++ jquery-7-8-9-10/scripts/main.js
@@ -4,8 +4,15 @@ $(function() {
   var $name = $('#name')
   var $drink = $('#drink')
 
+  var orderTemplate = '' +
+  '<li>' +
+  '<p><strong>Name:</strong> {{name}}</p>' +
+  '<p><strong>Drink:</strong> {{drink}}</p>' +
+  '<button data-id='{{id}}' class='remove'>X</button>' +
+  '</li>'
+
   function addOrder(order) {
-    $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>')
+    $orders.append(Mustache.render(orderTemplate, order))
   }
 
   $.ajax({
@@ -36,6 +43,20 @@ $(function() {
       },
       error: function() {
         alert('error saving order')
+      }
+    })
+  })
+
+  $orders.delegate('.remove', 'click', function() {
+
+    var $li = $(this).closest('li')
+    $.ajax({
+      type: 'DELETE',
+      url: '/api/orders/' + $(this).attr('data-id'),
+      success: function() {
+        $li.fadeOut(300, function() {
+          $(this).remove()
+        })
       }
     })
   })
